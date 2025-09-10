b01ff39fe2983c638e6720f7ee210540e0d5973d
Preeti <preeti.kumari241116@gmail.com>
2025-09-10 10:11:48 +0530

    Committing DB changed scripts

--- AllDBScriptsUserservice.sql
+++ AllDBScriptsUserservice.sql
@@ -1,3 +1,10 @@
+CREATE USER EcommerceApplicationDBUser;
+
+CREATE DATABASE productservicesept25db;
+GRANT ALL privileges ON productservicesept25db.* to 'EcommerceApplicationDBUser';
+FLUSH PRIVILEGES;
+
+
 CREATE DATABASE userservicesept25db;
-CREATE USER userservicesept25db;
-GRANT ALL privileges ON userservicesept25db.* to 'userservicesept25db';
+GRANT ALL privileges ON userservicesept25db.* to 'EcommerceApplicationDBUser';
+FLUSH PRIVILEGES;
