553576ea1ff6c1b6385c1abda789712a656731fd
Preeti <preeti.kumari241116@gmail.com>
2025-09-10 15:07:10 +0530

    Part 4-> Implement Login API using OAuth2 and Spring Security

--- src/main/java/com/scaler/userservicesept25/UserServiceSept25Application.java
+++ src/main/java/com/scaler/userservicesept25/UserServiceSept25Application.java
@@ -2,8 +2,10 @@ package com.scaler.userservicesept25;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UserServiceSept25Application {
 
     public static void main(String[] args) {
--- src/main/java/com/scaler/userservicesept25/models/BaseModel.java
+++ src/main/java/com/scaler/userservicesept25/models/BaseModel.java
@@ -1,27 +1,32 @@
 package com.scaler.userservicesept25.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO INCREMENT
     private Long id; // Unique identifier for the model
 
+    @CreatedDate
     private Date createdAt; // Timestamp for when the model was created
 
+    @LastModifiedDate
     private Date lastUpdatedAt; // Timestamp for when the model was last updated
 
+    @CreatedBy
     private String createdBy; // Identifier for the user who created the model
 
     private State state; // State of the model (e.g., ACTIVE, INACTIVE)
